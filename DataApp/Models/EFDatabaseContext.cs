3c6c85b7aa82b856d69ded0e027ed1a181dcd2b9
Вячеслав Кузькин <Slava835@live.com>
2020-06-05 08:43:59 +0300

    Добавьте файлы проекта.

---
+++ DataApp/Models/EFDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DataApp.Models
+{
+    public class EFDatabaseContext : DbContext
+    {
+        public EFDatabaseContext(DbContextOptions<EFDatabaseContext> opts)
+        : base(opts) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ DataApp/Models/SupplierRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace DataApp.Models
+{
+    public interface ISupplierRepository
+    {
+        Supplier Get(long id);
+        IEnumerable<Supplier> GetAll();
+        void Create(Supplier newDataObject);
+        void Update(Supplier changedDataObject);
+        void Delete(long id);
+    }
+    public class SupplierRepository : ISupplierRepository {
+        private EFDatabaseContext context;
+
+        public SupplierRepository(EFDatabaseContext ctx) => context = ctx;
+
+        public Supplier Get(long id) {
+            return context.Suppliers.Find(id);
+        }
+
+        public IEnumerable<Supplier> GetAll() {
+            return context.Suppliers;
+        }
+
+        public void Create(Supplier newDataObject) {
+            context.Add(newDataObject);
+            context.SaveChanges();
+        }
+
+        public void Update(Supplier chagedDataObject) {
+            context.Update(chagedDataObject);
+            context.SaveChanges();
+        }
+
+        public void Delete(long id) {
+            context.Remove(Get(id));
+            context.SaveChanges();
+        }
+
+    }
+}
