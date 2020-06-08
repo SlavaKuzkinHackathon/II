ce07a369339ee142b2c7f0f6b89168aa0c95f036
Вячеслав Кузькин <Slava835@live.com>
2020-06-08 07:10:28 +0300

    Working with Related Data in a One-to-Many Relationship_2

--- DataApp/Controllers/SuppliersController.cs
+++ DataApp/Controllers/SuppliersController.cs
@@ -4,23 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using DataApp.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace DataApp.Controllers
 {
     public class SuppliersController : Controller
     {
        private ISupplierRepository supplierRepository;
+        private EFDatabaseContext dbContext; 
         
 
-        public SuppliersController(ISupplierRepository supplierRepo) {
+        public SuppliersController(ISupplierRepository supplierRepo,
+            EFDatabaseContext context) {
             supplierRepository = supplierRepo;
+            dbContext = context;
         }
 
         public IActionResult Index()
         {
             ViewBag.SupplierEditId = TempData["SupplierEditId"];
             ViewBag.SupplierCreateId = TempData["SupplierCreateId"];
+            ViewBag.SupplierRelationshipId = TempData["SupplierRelationshipId"];
             return View(supplierRepository.GetAll());
         }
 
@@ -37,6 +41,54 @@ namespace DataApp.Controllers
 
         public IActionResult Create(long id) {
             TempData["SupplierCreateId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+
+        /* 
+        public IActionResult Change(long id)
+        {
+            TempData["SupplierRelationshipId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Change(Supplier supplier)
+        {
+            IEnumerable<Product> changed
+            = supplier.Products.Where(p => p.SupplierId != supplier.Id);
+            IEnumerable<long> targetSupplierIds
+            = changed.Select(p => p.SupplierId).Distinct();
+            if (changed.Count() > 0)
+            {
+                IEnumerable<Supplier> targetSuppliers = dbContext.Suppliers
+                .Where(s => targetSupplierIds.Contains(s.Id))
+                .AsNoTracking().ToArray();
+                foreach (Product p in changed)
+                {
+                    Supplier newSupplier
+                    = targetSuppliers.First(s => s.Id == p.SupplierId);
+                    newSupplier.Products = newSupplier.Products == null
+                    ? new Product[] { p }
+                    : newSupplier.Products.Append(p).ToArray();
+                }
+                dbContext.Suppliers.UpdateRange(targetSuppliers);
+                dbContext.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }*/
+
+
+        public IActionResult Change(long id)
+        {
+            TempData["SupplierRelationshipId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Change(long Id, Product[] products)
+        {
+            dbContext.Products.UpdateRange(products.Where(p => p.SupplierId != Id));
+            dbContext.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
     }
